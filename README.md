efeda1d41085edc68688a9c9751aae8340e73c87
Rauwomos <rw562@bath.ac.uk>
2016-01-12 17:07:01 +0000

    It works

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CSGOMusicPlayer
-Uses CSGO's game state integration to play and pause music.
+Uses CSGO's game state integration to play when you are dead, and pause it when you are alive
+
+Known issues:
+	If you are using spotify and an advert is playing when your state changes then it won't play/pause the music when it stops
+		Fixes:
+			play/pause the music when the advert has finished
+			pay for spotify premium so there are no adds
--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 import json
 import requests
+import win32api
+import logging
 from flask import Flask, session, request, current_app
 
-previous_health = 0
+# stops the app printing to console unlese there is an error
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+VK_MEDIA_PLAY_PAUSE = 0xB3
+hwcode = win32api.MapVirtualKey(VK_MEDIA_PLAY_PAUSE, 0)
+
+previous_health = 100
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def main():
-	json_data = json.loads(request.data)
+	global previous_health
+	
+	# Imports the json information and stores player information
+	json_data = json.loads(request.data.decode())
 	player = json_data.get('player', {})
 	steamid = player.get('steamid', '')
+
+	# Steamid of the player that is using the app. This stops the health that is read in from showing someone elses health when you are dead
 	if steamid == '76561198058071054':
+
+		#If the steamid is correct then it extracts the information about the players health fromt he json
 		player_state = player.get('state', '')
 		health = int(player_state.get('health', ''))
+
+		# If the player goes from being alive to dead or vice versa then it plays/pauses the music using the windows media key play/pause
 		if health == 0 and previous_health != 0:
-			# TODO play music
-			print 'dead'
+			win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, hwcode)
 		elif health != 0 and previous_health == 0:
-			# TODO pause music
-			print 'alive'
-		global previous_health
+			win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, hwcode)
 		previous_health = health
-		# print request.data
-	return ''
+	return 'you lost the game'
 
 if __name__ == "__main__":
-	app.run(port=3000, debug=True)
+	print('Running')
+	app.run(port=3000)
