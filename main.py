353655d30c3721935d1efab457e7f3a60d87325d
Mark McGuire <mark.b.mcg@gmail.com>
2016-01-12 22:19:32 -0600

    Various cleanup/improvements
    * use flask builtin json feature
    * use flask app globals (g) instead of a global variable
    * don't cause 500's when reading health
    * change return to OK
    * move steamid to static variable (want to move to argument, but have no idea how argparse and windows play together)

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
-import json
-import requests
 import pyautogui
 import logging
-from flask import Flask, session, request, current_app
+from flask import Flask, request, g
 
-# stops the app printing to console unlese there is an error
+TARGET_STEAM_ID = '76561198058071054'
+
+# stops the app printing to console unless there is an error
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
-# VK_MEDIA_PLAY_PAUSE = 0xB3
-# hwcode = win32api.MapVirtualKey(VK_MEDIA_PLAY_PAUSE, 0)
-
-previous_health = 100
-
 app = Flask(__name__)
+
 
 @app.route("/", methods=["POST"])
 def main():
-	global previous_health
-	
-	# Imports the json information and stores player information
-	json_data = json.loads(request.data.decode())
-	player = json_data.get('player', {})
-	steamid = player.get('steamid', '')
+    previous_health = g.get('previous_health', 100)
 
-	# Steamid of the player that is using the app. This stops the health that is read in from showing someone elses health when you are dead
-	if steamid == '76561198058071054':
+    # Imports the json information and stores player information
+    player = request.json.get('player', {})
 
-		#If the steamid is correct then it extracts the information about the players health fromt he json
-		player_state = player.get('state', '')
-		health = int(player_state.get('health', ''))
+    # Steamid of the player that is using the app. This stops the health that is read in from showing someone elses health when you are dead
+    if player.get('steamid', None) == TARGET_STEAM_ID:
+        # If the steamid is correct then it extracts the information about the players health fromt he json
+        player_state = player.get('state', {})
+        health = player_state.get('health', None)
 
-		# If the player goes from being alive to dead or vice versa then it plays/pauses the music using the windows media key play/pause
-		if health == 0 and previous_health != 0:
-			pyautogui.press('playpause')
-		elif health != 0 and previous_health == 0:
-			pyautogui.press('playpause')
-		previous_health = health
-	return 'you lost the game'
+        # If the player goes from being alive to dead or vice versa then it plays/pauses the music using the windows media key play/pause
+        if health == 0 and previous_health != 0:
+            pyautogui.press('playpause')
+        elif health != 0 and previous_health == 0:
+            pyautogui.press('playpause')
+        if health:
+            g.previous_health = health
+    return 'OK', 200
+
 
 if __name__ == "__main__":
-	print('Running')
-	app.run(port=3000)
+    print('Running')
+    app.run(port=3000)
