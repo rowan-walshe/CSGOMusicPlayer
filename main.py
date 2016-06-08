9cf6bd20776a3b0a1de7fbe2174b4cf1e27aae89
Rauwomos <rw562@bath.ac.uk>
2016-06-08 19:39:35 +0100

    Fixed issue with CS: GO not receiving HTTP 200 status

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyautogui
 import logging
+import json
 from flask import Flask, request
 
 TARGET_STEAM_ID = '76561198058071054'
@@ -15,26 +16,29 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def main():
-	global previous_health
-	# Imports the json information and stores player information
-	player = request.json.get('player', {})
 
-	# Steamid of the player that is using the app. This stops the health that is read in from showing someone elses health when you are dead
-	if player.get('steamid', None) == TARGET_STEAM_ID:
+    global previous_health
 
-		# If the steamid is correct then it extracts the information about the players health fromt he json
-		player_state = player.get('state', {})
-		health = player_state.get('health', None)
+    # Imports the json information and stores player information
+    player = request.json.get('player', {})
 
-		# If the player goes from being alive to dead or vice versa then it plays/pauses the music using the windows media key play/pause4
-		if health == 0 and previous_health != 0:
-			pyautogui.press('playpause')
-		elif health != 0 and previous_health == 0:
-			pyautogui.press('playpause')
-		previous_health = health
-	return 'OK', 200
+    # Steamid of the player that is using the app. This stops the health that is read in from showing someone elses health when you are dead
+    if player.get('steamid', None) == TARGET_STEAM_ID:
+        # If the steamid is correct then it extracts the information about the players health fromt he json
+        player_state = player.get('state', {})
+        health = player_state.get('health', None)
+
+        # If the player goes from being alive to dead or vice versa then it plays/pauses the music using the windows media key play/pause4
+        if health == 0 and previous_health != 0:
+            pyautogui.press('playpause')
+            print("Died")
+        elif health != 0 and previous_health == 0:
+            pyautogui.press('playpause')
+            print("Alive")
+        previous_health = health
+    return ("", 200)
 
 
 if __name__ == "__main__":
-	print('Running')
-	app.run(port=3000)
+    print('Running')
+    app.run(port=3000)
